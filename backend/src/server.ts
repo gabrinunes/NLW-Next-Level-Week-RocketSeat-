686af0b495897894d147022a42cfffdd479db990
Gabriel Cunha <gn_cunha@hotmail.com>
2020-06-02 12:09:48 -0300

    config inicial

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express, { request, response } from 'express';
-import routes from './routes'
+import Routes from './routes'
 const app = express();
 
 app.use(express.json())
-app.use(routes)
+app.use(Routes)
 
 app.listen(3333)
