5e90f238c951df0a1a5bcc9f73dedf9813cd4609
Gabriel Cunha <gn_cunha@hotmail.com>
2020-06-02 23:31:17 -0300

    backend finalizado

---
+++ backend/src/database/migrations/02_create_points_items.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex'
+
+export async function up (knex:Knex){
+   return knex.schema.createTable('point_items',table=>{
+       table.increments('id').primary();
+
+       table.string('point_id')
+       .notNullable()
+       .references('id')
+       .inTable('points');
+
+
+       table.string('item_id')
+       .notNullable()
+       .references('id')
+       .inTable('items');
+   });
+}
+
+export async function down(kenx:Knex){
+  return kenx.schema.dropTable('point_items');
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import express  from 'express'
-
+import PointsController from './controllers/PointsController'
+import ItensController from './controllers/ItemsController'
 const routes = express.Router()
+const pointsController = new PointsController();
+const itensCotroller = new ItensController();
 
-routes.get('/', (request,response)=>{
-    return response.json({message:'Hello World'});
-});
+routes.get('/items',itensCotroller.index)
+
+routes.post('/points',pointsController.create)
+routes.get('/points/',pointsController.index)
+routes.get('/points/:id',pointsController.show)
 
 export default routes;
