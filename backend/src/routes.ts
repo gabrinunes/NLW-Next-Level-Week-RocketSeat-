76f34e08bd917afca5fb8e625861417052759e78
Gabriel Cunha <gn_cunha@hotmail.com>
2020-06-05 20:30:35 -0300

    validações no back-end

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,38 @@
 import express  from 'express'
+import {celebrate,Joi} from 'celebrate'
+import multer from 'multer'
+import multerConfig from './config/multer'
+
 import PointsController from './controllers/PointsController'
 import ItensController from './controllers/ItemsController'
+
 const routes = express.Router()
 const pointsController = new PointsController();
 const itensCotroller = new ItensController();
 
 routes.get('/items',itensCotroller.index)
+const upload = multer(multerConfig)
 
-routes.post('/points',pointsController.create)
 routes.get('/points/',pointsController.index)
 routes.get('/points/:id',pointsController.show)
+
+routes.post('/points',
+upload.single('image'),
+celebrate({
+    body:Joi.object().keys({
+        name:Joi.string().required(),
+        email:Joi.string().required(),
+        whatsapp:Joi.string().required(),
+        latitude:Joi.number().required(),
+        longitude:Joi.number().required(),
+        city:Joi.string().required(),
+        uf:Joi.string().required().max(2),
+        items:Joi.string().required(),
+    })
+},{
+    abortEarly:false
+}),
+pointsController.create
+)
 
 export default routes;
--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -5,6 +5,7 @@ import {Map,TileLayer,Marker} from 'react-leaflet'
 import {LeafletMouseEvent} from 'leaflet'
 import api from '../../services/api'
 import axios from 'axios'
+import Dropzone from '../../components/Dropzone'
 import './styles.css'
 
 import logo from '../../assets/logo.svg'
@@ -39,6 +40,7 @@ const CreatePoint =()=>{
     const [selectedCity,setSelectedCity] = useState('0')
     const [selectedItems,setSelectedItems] = useState<number[]>([])
     const [selectedPosition,setSelectedPosition]=useState<[number,number]>([0,0])
+    const [selectedFile,setSelectedFile] = useState<File>()
 
     const history = useHistory()
 
@@ -118,17 +120,22 @@ const CreatePoint =()=>{
         const city = selectedCity;
         const [latitude,longitude] = selectedPosition;
         const items = selectedItems;
+
+        const data = new FormData()
         
-        const data = {
-           name,
-           email,
-           whatsapp,
-           uf,
-           city,
-           latitude,
-           longitude,
-           items
-        };
+           data.append('name',name)
+           data.append('email',email)
+           data.append('whatsapp',whatsapp)
+           data.append('uf',uf)
+           data.append('city',city)
+           data.append('latitude',String(latitude))
+           data.append('longitude',String(longitude))
+           data.append('items',items.join(','))
+
+           if(selectedFile){
+               data.append('image',selectedFile)
+           }
+
         await api.post('points',data)
 
         alert('Ponto de coleta criado!')
@@ -148,7 +155,9 @@ const CreatePoint =()=>{
 
            <form onSubmit={handleSubmit}>
                <h1>Cadastro do <br/> ponto de coleta</h1>
-
+                
+                <Dropzone onFileUploaded={setSelectedFile}/>
+               
                <fieldset>
                    <legend>
                        <h2>Dados</h2>
